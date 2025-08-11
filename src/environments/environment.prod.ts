790f16a2ec28b24b09cc2d1b213a2d408afa5947
Chamith <chamith.softwaredev@gmail.com>
2025-08-11 16:43:57 +0530

    adding changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   defaultauth: "fackbackend",
-  apiUrl: "https://synerisqcback-end-production.up.railway.app",
+  apiUrl: "https://qc-sample-backend-production.up.railway.app",
   bucketUrl: "https://upload.gofile.io",
 };
